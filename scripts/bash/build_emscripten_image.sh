4ae851c0ae78a898526cac04da84d6653613c2f6
Bartek Wrona <wrona@syncad.com>
2023-10-18 19:13:48 +0200

    emscripten sdk base image upgraded to 3.1.47 version

--- scripts/bash/build_emscripten_image.sh
+++ scripts/bash/build_emscripten_image.sh
@@ -5,11 +5,11 @@ SCRIPTSDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SRCDIR="${SCRIPTSDIR}/../../"
 
 REGISTRY=${1:-"registry.gitlab.syncad.com/hive/common-ci-configuration/"}
-EMSDK_VERSION=${1:-"3.1.43"}
+EMSDK_VERSION=${1:-"3.1.47"}
 
 export DOCKER_BUILDKIT=1
 
 docker build --target=emscripten_builder \
   --build-arg "EMSCRIPTEN_VERSION=${EMSDK_VERSION}" \
-  --tag "${REGISTRY}emsdk:3.1.43" \
+  --tag "${REGISTRY}emsdk:${EMSDK_VERSION}" \
   --file "${SRCDIR}/Dockerfile.emscripten" "${SRCDIR}"
