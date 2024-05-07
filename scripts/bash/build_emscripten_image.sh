aa7e265adac7716704c1d6715d48b248ad15e94d
Bartek Wrona <wrona@syncad.com>
2024-05-07 23:03:52 +0200

    emsdk image supplemented by helper npm scripts, simplifying common CI steps implementation

--- scripts/bash/build_emscripten_image.sh
+++ scripts/bash/build_emscripten_image.sh
@@ -9,7 +9,7 @@ EMSDK_VERSION=${1:-"3.1.56"}
 
 export DOCKER_BUILDKIT=1
 
-docker build --target=emscripten_builder \
+docker build --target=supplemented_emscripten_builder \
   --build-arg "EMSCRIPTEN_VERSION=${EMSDK_VERSION}" \
-  --tag "${REGISTRY}emsdk:${EMSDK_VERSION}" \
+  --tag "${REGISTRY}emsdk:${EMSDK_VERSION}-1" \
   --file "${SRCDIR}/Dockerfile.emscripten" "${SRCDIR}"
