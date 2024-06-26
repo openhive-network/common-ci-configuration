e926485a5ce59f4cd408ec52c40c2f1a097c9692
Bartek Wrona <wrona@syncad.com>
2024-06-26 18:10:06 +0200

    DIST_TAG value forwarded to npm_generate_version.sh call.

--- scripts/bash/npm-helpers/npm_build_package.sh
+++ scripts/bash/npm-helpers/npm_build_package.sh
@@ -8,10 +8,11 @@ REGISTRY_URL="${2:?Missing arg #2 pointing an NPM registry URL}"
 SCOPE="${3:?Missing arg #3 pointing a package scope}"
 PROJECT_NAME="${4:?Missing arg #4 pointing a project name}"
 OUTPUT_DIR="${5:?Missing arg #5 pointing an output directory}"
+DIST_TAG="${6:-dev}"
 
 pushd "${SOURCE_DIR}" # move to the project directory (where package.json file is located)
 
-"${SCRIPTPATH}/npm_generate_version.sh" "${SOURCE_DIR}" "${REGISTRY_URL}" "${SCOPE}" "${PROJECT_NAME}"
+"${SCRIPTPATH}/npm_generate_version.sh" "${SOURCE_DIR}" "${REGISTRY_URL}" "${SCOPE}" "${PROJECT_NAME}" "${DIST_TAG}"
 
 npm run build
 npm pack --pack-destination "${OUTPUT_DIR}" --json > "${OUTPUT_DIR}/built_package_info.json"
