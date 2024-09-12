9ec7fd2e0a2491ed808aa1358788a786db9ec035
mtyszczak <mateusz.tyszczak@gmail.com>
2024-09-12 11:14:04 +0200

    Add npm provenance enable flag for production npm build

--- scripts/bash/npm-helpers/npm_publish.sh
+++ scripts/bash/npm-helpers/npm_publish.sh
@@ -7,6 +7,9 @@ PROJECT_DIR="${1:?Missing arg #1 specifying a project source directory}"
 REGISTRY_URL="${2:?Missing arg #2 pointing an NPM registry URL}"
 SCOPE="${3:?Missing arg #3 pointing a package scope}"
 PUBLISH_TOKEN="${4:?Missing arg #4 pointing a deployment token}"
+NPM_PROVENANCE_ENABLE="${5:-0}"
+
+NPM_FLAGS="--access=public"
 
 echo> "${PROJECT_DIR}/.npmrc"
 
@@ -27,9 +30,15 @@ if [ "${VERSION}" = "" ]; then
   exit 1
 fi
 
+NPM_FLAGS="${NPM_FLAGS} --tag \"${PACKAGE_DIST_TAG}\""
+
 set +e
 
 echo "Attempting to verify presence of package: ${NAME}@${VERSION}, dist-tag: ${PACKAGE_DIST_TAG} in the registry: ${REGISTRY_URL}"
+
+if [ "${NPM_PROVENANCE_ENABLE}" -eq "1" ]; then
+  NPM_FLAGS="${NPM_FLAGS} --provenance"
+fi
 
 # Check if package with given version has been already published
 npm view "${NAME}@${VERSION}" version 2>/dev/null
@@ -40,7 +49,7 @@ else
   set -e
   echo "Publishing ${NAME}@${VERSION} to tag ${PACKAGE_DIST_TAG}"
   # We are going to repack the tarball as there are registry-dependent data in each job for package.json
-  npm publish --access=public --tag "${PACKAGE_DIST_TAG}"
+  npm publish $NPM_FLAGS
 fi
 
 popd
