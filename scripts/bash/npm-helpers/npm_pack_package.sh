c94317a5d3542d8e9a28a15db70340152086068b
mtyszczak <mateusz.tyszczak@gmail.com>
2025-02-19 13:50:03 +0100

    Fix pnpm pack output generation skipping pnpm echo lines

--- scripts/bash/npm-helpers/npm_pack_package.sh
+++ scripts/bash/npm-helpers/npm_pack_package.sh
@@ -15,11 +15,11 @@ pushd "${SOURCE_DIR}" # move to the project directory (where package.json file i
 
 "${SCRIPTPATH}/npm_generate_version.sh" "${SOURCE_DIR}" "${REGISTRY_URL}" "${SCOPE}" "${PROJECT_NAME}" "${COMMIT_REF_PROTECTED}" "${COMMIT_TAG}"
 
-pnpm pack --pack-destination "${OUTPUT_DIR}" --json > "${OUTPUT_DIR}/built_package_info.json"
-BUILT_PACKAGE_NAME=$(jq -r .[].filename "${OUTPUT_DIR}/built_package_info.json")
+pnpm pack --pack-destination "${OUTPUT_DIR}" --json | tail -n +5 > "${OUTPUT_DIR}/built_package_info.json"
+BUILT_PACKAGE_NAME=$(jq -r .filename "${OUTPUT_DIR}/built_package_info.json")
 {
   echo PACKAGE_SOURCE_DIR="${SOURCE_DIR}"
-  echo BUILT_PACKAGE_PATH="${OUTPUT_DIR}/${BUILT_PACKAGE_NAME}"
+  echo BUILT_PACKAGE_PATH="${BUILT_PACKAGE_NAME}"
 } > "${SOURCE_DIR}/built_package_info.env"
 
 echo "built_package_info.env file contents:"
