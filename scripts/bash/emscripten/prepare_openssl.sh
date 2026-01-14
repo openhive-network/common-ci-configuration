f2db7515fe4bc17ff96a98a522c025d29c113473
Bartek Wrona <bwrona@syncad.com>
2026-01-14 18:10:17 +0000

    Optimize OpenSSL build for minimal WASM size
    Disable unused OpenSSL features to reduce library size:
    - Disable protocols: SSL, TLS, DTLS, SSL3
    - Disable unused algorithms: DSA, DH, EC2M, SM2/3/4, IDEA, MDC2,
      RC2/4/5, Blowfish, CAST, Camellia, SEED, ARIA, ChaCha, Poly1305,
      SIPHASH, Whirlpool, Scrypt
    - Disable unused features: CMS, OCSP, SRP, PSK, timestamps, CT, compression
    - Disable I/O: stdio, sockets
    - Disable error strings: no-err, no-autoerrinit, no-filenames
    
    Add build optimizations:
    - -fvisibility=hidden to hide internal symbols
    - -ffunction-sections -fdata-sections with --gc-sections for dead code elimination

--- scripts/bash/emscripten/prepare_openssl.sh
+++ scripts/bash/emscripten/prepare_openssl.sh
@@ -14,20 +14,55 @@ cd "${TMP_SRC}/openssl"
 emconfigure ./Configure \
   --prefix="${INSTALL_PREFIX}" \
   --openssldir="${INSTALL_PREFIX}" \
-  no-hw \
   no-shared \
   no-asm \
   no-threads \
   no-ssl3 \
   no-dtls \
+  no-dtls1 \
   no-engine \
   no-dso \
+  no-dsa \
+  no-dh \
+  no-ec2m \
+  no-sm2 \
+  no-sm3 \
+  no-sm4 \
+  no-idea \
+  no-mdc2 \
+  no-rc2 \
+  no-rc4 \
+  no-rc5 \
+  no-bf \
+  no-cast \
+  no-camellia \
+  no-seed \
+  no-aria \
+  no-chacha \
+  no-poly1305 \
+  no-siphash \
+  no-whirlpool \
+  no-scrypt \
+  no-cms \
+  no-ocsp \
+  no-srp \
+  no-psk \
+  no-ts \
+  no-ct \
+  no-comp \
+  no-ssl \
+  no-tls \
+  no-stdio \
+  no-sock \
+  no-filenames \
+  no-autoerrinit \
+  no-err \
   linux-x32 \
   -static
 
 # shellcheck disable=SC2016
 sed -i 's/$(CROSS_COMPILE)//' Makefile
-emmake make -j 8 CFLAGS="-Oz" CXXFLAGS="-Oz" LDFLAGS="-Oz"
+emmake make -j 8 CFLAGS="-Oz -fvisibility=hidden -ffunction-sections -fdata-sections" CXXFLAGS="-Oz -fvisibility=hidden -ffunction-sections -fdata-sections" LDFLAGS="-Oz --gc-sections"
 emmake make install
 
 echo "OpenSSL build finished."
