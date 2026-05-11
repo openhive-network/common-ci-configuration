cc96cc355a81c485f8d84f7331c74afd63c0fd17
Bartek Wrona <wrona@syncad.com>
2026-05-11 17:57:37 +0200

    Updated preinstalled version of playwright.

--- scripts/bash/build_emscripten_image.sh
+++ scripts/bash/build_emscripten_image.sh
@@ -5,12 +5,12 @@ SCRIPTSDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SRCDIR="${SCRIPTSDIR}/../../"
 
 REGISTRY=${1:-"registry.gitlab.syncad.com/hive/common-ci-configuration/"}
-EMSDK_VERSION=${1:-"4.0.22"}
+EMSDK_VERSION=${1:-"5.0.2"}
 
 export DOCKER_BUILDKIT=1
 
 docker build --target=supplemented_emscripten_builder \
   --progress=plain \
   --build-arg "EMSCRIPTEN_VERSION=${EMSDK_VERSION}" \
-  --tag "${REGISTRY}emsdk:${EMSDK_VERSION}-6" \
+  --tag "${REGISTRY}emsdk:${EMSDK_VERSION}-3" \
   --file "${SRCDIR}/Dockerfile.emscripten" "${SRCDIR}"
